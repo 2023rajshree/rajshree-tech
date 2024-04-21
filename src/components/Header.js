6b8f7f7bd76d6bec83f57e0e43384c042123e64c
S Tungare <tungares@hotmail.com>
2024-04-21 19:33:00 +0530

    Added: Header and footer

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+export default function Header() {
+    return (
+        <>
+            {/* <script src="https://sdk.scdn.co/spotify-player.js"></script> */}
+            {/* This is header */}
+            <header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Connect</a></li>
+                </ul>
+            </nav>
+            </header>
+    
+        </>
+    )
+}
